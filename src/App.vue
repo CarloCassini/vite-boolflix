c5368ab9b8b53e419c572459ed13e2588e34866e
CarloCassini <c.cassini87@gmail.com>
2023-09-14 16:54:50 +0200

    initialize

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+// importo axios
+import axios from "axios";
+// importo lo store
+import { store } from "./data/store.js";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {},
+
+  // created: {},
+
+  props: {},
+
+  emits: [],
+};
+</script>
+
+<template>
+  <h1 class="container">
+    we
+
+    <font-awesome-icon icon="paper-plane" class="me-5" />
+  </h1>
+</template>
+
+<style lang="scss"></style>
